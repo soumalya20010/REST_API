6f1ed8dfc0ceddf6627872a55a4baf486376744d
Soumalya Biswas <soumalyaabiswas2001@gmail.com>
2025-01-30 18:17:53 +0530

    server

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import dotenv from 'dotenv';
 import { sequelize } from './models/index.js';
 import app from './app.js';
 app.use(express.json());
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
