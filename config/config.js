4b42ce2765d93f2d816c315bf64b8f829e92d0c5
Soumalya Biswas <soumalyaabiswas2001@gmail.com>
2025-01-30 18:50:54 +0530

    new

--- config/config.js
+++ config/config.js
@@ -2,15 +2,21 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-  development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
-  },
- 
-};
-
+    development: {
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: 'postgres',
+    },
+    production: {
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: 'postgres',
+    },
+  };
 export default config;
